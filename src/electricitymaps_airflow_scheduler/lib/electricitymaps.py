c711007e36522caf520b549e3a74f9c4b25cd708
Pierre Segonne <pierre.segonne@electricitymaps.com>
2025-09-18 10:04:05 +0200

    feat: setup project

---
+++ src/electricitymaps_airflow_scheduler/lib/electricitymaps.py
@@ -0,0 +1,6 @@
+"""
+Here we will have the logic to call Electricity Maps' API to schedule operations within an Airflow pipeline
+"""
+
+BASE_URL = "https://api.electricitymaps.com/"
+API_VERSION = "v3"
